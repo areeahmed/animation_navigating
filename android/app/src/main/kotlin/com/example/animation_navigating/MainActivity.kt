ad707a46a3bc74396059734081ae45341e5c1c74
Ari Ahmed <59537867+areeahmed@users.noreply.github.com>
2023-01-13 13:07:43 +0300

    first push adding page animation transition

---
+++ android/app/src/main/kotlin/com/example/animation_navigating/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.animation_navigating
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:animation_navigating/src/myapp.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
